5c1c14e05ee7fa4a88e1c55970d779a5e6e00a60
GiseEveRam <evelynmdp@hotmail.com>
2024-06-17 07:48:14 -0300

    Creacion del Navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"; 
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar";
 import Login from "./pages/Login";
 import "./App.css";
+//import { Navbar } from "react-bootstrap";
 
 const App = () => {
   return (
