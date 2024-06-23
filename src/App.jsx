e10d56a1421cf9071fa7a168a386ed399400fdb1
GiseEveRam <evelynmdp@hotmail.com>
2024-06-23 08:16:10 -0300

    cambios en el paq

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar"; 
-//import Cards from "./components/Info/Cards";
-//import Cards from "./components/Info/Tablas";
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
 import { UserContext } from "./contexts";
+//import Card from './components/info/Card';
+//import Table from './components/info/Tablas';
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
 
@@ -14,17 +14,20 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 const App = () => {
   const [isAuthenticated, setIsAuthenticated] = useState(false);
   const [user, setUser] = useState(null);
+ 
+
+ 
 
   return (
     <UserContext.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser }}>
       <Router>
       <Navbar/>
-      <Cards/>
-      <Tablas/>
         <Routes>
           <Route path="/" element={<Home />} />
           <Route path="/login" element={<Login />} />
+          
           <Route path="*" element={<NotFound />} />
+          
         </Routes>
       </Router>
     </UserContext.Provider>
