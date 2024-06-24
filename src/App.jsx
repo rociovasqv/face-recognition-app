eb91270bc0101cdcd24fb13247078bf424c0f9b5
Stefano Ferrari <stefanoferrari.dev@gmail.com>
2024-06-23 23:51:16 -0300

    Quick fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,16 @@
-import React, { useState } from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./pages/Home";
 import Navbar from "./components/Navbar"; 
 import Login from "./pages/Login";
 import NotFound from "./pages/NotFound";
-import { UserContext } from "./contexts";
 //import Card from './components/info/Card';
-
 import "./App.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import { Card } from "react-bootstrap";
 
 
 const App = () => {
-  const [isAuthenticated, setIsAuthenticated] = useState(false);
-  const [user, setUser] = useState(null);
   return (
-    <UserContext.Provider value={{ isAuthenticated, setIsAuthenticated, user, setUser }}>
+    <AuthProvider>
       <Router>
       <Navbar/>
         <Routes>
@@ -25,10 +19,9 @@ const App = () => {
           {/* <Route path="/" component={Card} /> */}
           {/* <Route path="/table" element={<Table records={records} />} /> */}
           <Route path="*" element={<NotFound />} />
-          
         </Routes>
       </Router>
-    </UserContext.Provider>
+    </AuthProvider>
   );
 };
 
