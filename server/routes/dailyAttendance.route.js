4291738dec7a246aea5419db7dc6623d831987ff
Stefano Ferrari <stefanoferrari.dev@gmail.com>
2024-06-30 20:19:15 -0300

    quick fix backend

--- server/routes/dailyAttendance.route.js
+++ server/routes/dailyAttendance.route.js
@@ -13,7 +13,7 @@ router.post("/mark-present", dailyAttendanceController.markUserPresent);
 router.post("/", dailyAttendanceController.create);
 router.get("/:id", dailyAttendanceController.getById);
 router.get("/date/:date", dailyAttendanceController.getByDatePopulate);
-router.get("/", authenticateToken, dailyAttendanceController.getAll);
+router.get("/", dailyAttendanceController.getAll);
 router.put("/:id", dailyAttendanceController.update);
 router.delete("/:id", dailyAttendanceController.delete);
 
